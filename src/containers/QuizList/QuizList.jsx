30e8cbf7c525488041234715430db3f2af6e0cf9
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-02-16 19:49:18 +0400

    create quiz list page

--- src/containers/QuizList/QuizList.jsx
+++ src/containers/QuizList/QuizList.jsx
@@ -1,11 +1,26 @@
 import React, { Component } from "react";
+import classes from "./QuizList.css";
+import { NavLink } from "react-router-dom";
 
 class QuizList extends Component {
   state = {};
+  renderQuizes() {
+    return [1, 2, 3].map((quiz, index) => {
+      return (
+        <li key={index}>
+          <NavLink to={"/quiz/" + quiz}>Test {quiz}</NavLink>
+        </li>
+      );
+    });
+  }
   render() {
     return (
-      <div>
-        <h1>Quiz List</h1>
+      <div className={classes.quizList}>
+        <div>
+          <h1>Quiz List</h1>
+
+          <ul>{this.renderQuizes()}</ul>
+        </div>
       </div>
     );
   }
