41086a2e5864be189dd63254e43de47fe8c91e87
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-02-16 19:53:44 +0400

    create a link thru a custom component

--- src/components/FinishedQuiz/FinishedQuiz.jsx
+++ src/components/FinishedQuiz/FinishedQuiz.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import classes from "./FinishedQuiz.css";
 import Button from "../ui/Button/Button";
+import { Link } from "react-router-dom";
 
 const FinishedQuiz = props => {
   const successCount = Object.keys(props.results).reduce((total, key) => {
@@ -35,7 +36,9 @@ const FinishedQuiz = props => {
         <Button onClick={props.onRetry} type="primary">
           Repeat
         </Button>
-        <Button type="success">Go to tests list</Button>
+        <Link to="/">
+          <Button type="success">Go to tests list</Button>
+        </Link>
       </div>
     </div>
   );
