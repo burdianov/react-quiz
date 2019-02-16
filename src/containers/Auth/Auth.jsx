5e9e71a80441e9e03c2b6114b160c711e8c517e7
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-02-16 21:39:59 +0400

    create auth page

--- src/containers/Auth/Auth.jsx
+++ src/containers/Auth/Auth.jsx
@@ -1,11 +1,31 @@
 import React, { Component } from "react";
+import classes from "./Auth.css";
+import Button from "../../components/ui/Button/Button";
 
 class Auth extends Component {
   state = {};
+  loginHandler = () => {};
+  registerHandler = () => {};
+  submitHandler = event => {
+    event.preventDefault();
+  };
   render() {
     return (
-      <div>
-        <h1>Auth</h1>
+      <div className={classes.auth}>
+        <div>
+          <h1>Authorization</h1>
+          <form onSubmit={this.submitHandler} className={classes.authForm}>
+            <input type="text" />
+            <input type="text" />
+
+            <Button type="success" onClick={this.loginHandler}>
+              Log in
+            </Button>
+            <Button type="primary" onClick={this.signupHandler}>
+              Sign up
+            </Button>
+          </form>
+        </div>
       </div>
     );
   }
