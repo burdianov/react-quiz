1be8f06f6341cba966b3753409ff789f5b819ad7
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-02-19 22:43:18 +0400

    form and components validation

---
+++ src/components/ui/Select/Select.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import classes from "./Select.css";
+
+const Select = props => {
+  const htmlFor = `${props.label}-${Math.random()}`;
+
+  return (
+    <div className={classes.select}>
+      <label htmlFor={htmlFor}>{props.label}</label>
+      <select id={htmlFor} value={props.value} onChange={props.onChange}>
+        {props.options.map((option, index) => {
+          return (
+            <option value={option.value} key={option.value + index}>
+              {option.text}
+            </option>
+          );
+        })}
+      </select>
+    </div>
+  );
+};
+
+export default Select;
