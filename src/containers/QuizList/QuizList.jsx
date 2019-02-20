5f94c749aa262857db4fe5d0225daa12ebe59a3b
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-02-20 22:05:29 +0400

    install axios and create firebase db

--- src/containers/QuizList/QuizList.jsx
+++ src/containers/QuizList/QuizList.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import classes from "./QuizList.css";
 import { NavLink } from "react-router-dom";
+import axios from "axios";
 
 class QuizList extends Component {
   state = {};
@@ -13,6 +14,15 @@ class QuizList extends Component {
       );
     });
   }
+
+  componentDidMount() {
+    axios
+      .get("https://react-quiz-ce6a7.firebaseio.com/quiz.json")
+      .then(response => {
+        console.log(response);
+      });
+  }
+
   render() {
     return (
       <div className={classes.quizList}>
