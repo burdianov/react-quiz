a617f094c8938651d9448b9f27bfabc56e90824c
Lilian Burdianov <lilian.burdianov@gmail.com>
2019-02-26 22:16:08 +0400

    refactor quiz page to redux - part 1

--- src/containers/quiz/Quiz.jsx
+++ src/containers/quiz/Quiz.jsx
@@ -4,17 +4,10 @@ import ActiveQuiz from "../../components/ActiveQuiz/ActiveQuiz";
 import FinishedQuiz from "../../components/FinishedQuiz/FinishedQuiz";
 import axios from "../../axios/axios-quiz";
 import Loader from "../../components/ui/Loader/Loader";
+import { connect } from "react-redux";
+import { fetchQuizById } from "../../store/actions/quiz";
 
 class Quiz extends Component {
-  state = {
-    results: {},
-    isFinished: false,
-    activeQuestion: 0,
-    answerState: null,
-    quiz: [],
-    loading: true
-  };
-
   onAnswerClickHandler = answerId => {
     if (this.state.answerState) {
       const key = Object.keys(this.state.answerState)[0];
@@ -67,20 +60,8 @@ class Quiz extends Component {
     });
   };
 
-  async componentDidMount() {
-    try {
-      const response = await axios.get(
-        `quizes/${this.props.match.params.id}.json`
-      );
-      const quiz = response.data;
-
-      this.setState({
-        quiz,
-        loading: false
-      });
-    } catch (e) {
-      console.log(e);
-    }
+  componentDidMount() {
+    this.props.fetchQuizById(this.props.match.params.id);
   }
 
   render() {
@@ -89,22 +70,22 @@ class Quiz extends Component {
         <div className={classes.quizWrapper}>
           <h1>Answer all the questions</h1>
 
-          {this.state.loading ? (
+          {this.props.loading || !this.props.quiz ? (
             <Loader />
-          ) : this.state.isFinished ? (
+          ) : this.props.isFinished ? (
             <FinishedQuiz
-              results={this.state.results}
-              quiz={this.state.quiz}
+              results={this.props.results}
+              quiz={this.props.quiz}
               onRetry={this.retryHandler}
             />
           ) : (
             <ActiveQuiz
-              question={this.state.quiz[this.state.activeQuestion].question}
-              answers={this.state.quiz[this.state.activeQuestion].answers}
+              question={this.props.quiz[this.props.activeQuestion].question}
+              answers={this.props.quiz[this.props.activeQuestion].answers}
               onAnswerClick={this.onAnswerClickHandler}
-              quizLength={this.state.quiz.length}
-              questionNumber={this.state.activeQuestion + 1}
-              state={this.state.answerState}
+              quizLength={this.props.quiz.length}
+              questionNumber={this.props.activeQuestion + 1}
+              state={this.props.answerState}
             />
           )}
         </div>
@@ -113,4 +94,24 @@ class Quiz extends Component {
   }
 }
 
-export default Quiz;
+function mapStateToProps(state) {
+  return {
+    results: state.quiz.results,
+    isFinished: state.quiz.isFinished,
+    activeQuestion: state.quiz.activeQuestion,
+    answerState: state.quiz.answerState,
+    quiz: state.quiz.quiz,
+    loading: state.quiz.loading
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    fetchQuizById: id => dispatch(fetchQuizById(id))
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Quiz);
--- src/store/reducers/quiz.js
+++ src/store/reducers/quiz.js
@@ -1,13 +1,19 @@
 import {
   FETCH_QUIZES_START,
   FETCH_QUIZES_SUCCESS,
-  FETCH_QUIZES_ERROR
+  FETCH_QUIZES_ERROR,
+  FETCH_QUIZ_SUCCESS
 } from "../actions/actionTypes";
 
 const initialState = {
   quizes: [],
   loading: false,
-  error: null
+  error: null,
+  results: {},
+  isFinished: false,
+  activeQuestion: 0,
+  answerState: null,
+  quiz: null
 };
 
 export default function quizReducer(state = initialState, action) {
@@ -28,6 +34,12 @@ export default function quizReducer(state = initialState, action) {
         ...state,
         loading: false,
         error: action.error
+      };
+    case FETCH_QUIZ_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        quiz: action.quiz
       };
     default:
       return state;
